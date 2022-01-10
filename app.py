f5d6e296e02d738ce3646a915b732e40f72304f3
Sumedha Shetty <sumedhashetty@Sumedhas-MacBook-Pro.local>
2022-01-10 20:59:29 +0530

    Fix nltk pop-up bug

--- app.py
+++ app.py
@@ -9,15 +9,15 @@ from flask import Flask , render_template , request
 
 # todo:Add imports
 import nltk
-import ssl
-try:
-    _create_unverified_https_context = ssl._create_unverified_context
-except AttributeError:
-    pass
-else:
-    ssl._create_default_https_context = _create_unverified_https_context
+# import ssl
+# try:
+#     _create_unverified_https_context = ssl._create_unverified_context
+# except AttributeError:
+#     pass
+# else:
+#     ssl._create_default_https_context = _create_unverified_https_context
 
-nltk.download()
+# nltk.download()
 
 # nltk.download('stopwords')
 
